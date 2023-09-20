1644d7ed1d650e6db5e6f367556871819f8287c4
Code with Bahri <codewithbahri@gmail.com>
2023-09-20 08:10:33 +0700

    listview

--- lib/main.dart
+++ lib/main.dart
@@ -12,52 +12,67 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: RowWidget(),
+      home: ListViewWidget(),
     );
   }
 }
 
-class RowWidget extends StatelessWidget {
-  const RowWidget({Key? key}) : super(key: key);
+class ListViewWidget extends StatelessWidget {
+  ListViewWidget({Key? key}) : super(key: key);
+
+  final List<String> categories = [
+    'All',
+    'Living Room',
+    'Bed Room',
+    'Dining Room',
+    'Kitchen'
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Row"),
+        title: const Text("ListView"),
       ),
       body: Container(
         padding: const EdgeInsets.all(10.0),
         child: Column(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    borderRadius: BorderRadius.circular(12.0),
-                  ),
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.arrow_back_ios),
-                  ),
+            SizedBox(
+              height: 32,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: List.generate(
+                  categories.length,
+                  (index) {
+                    return GestureDetector(
+                      onTap: () {},
+                      child: AnimatedContainer(
+                        duration: const Duration(milliseconds: 150),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.grey,
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 8,
+                          horizontal: 24,
+                        ),
+                        margin: const EdgeInsets.symmetric(horizontal: 2),
+                        child: Text(
+                          categories[index],
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            fontSize: 12,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500,
+                            letterSpacing: 1,
+                          ),
+                        ),
+                      ),
+                    );
+                  },
                 ),
-                const Text(
-                  "Detail",
-                  style: TextStyle(
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.normal,
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.share,
-                    size: 32.0,
-                  ),
-                ),
-              ],
+              ),
             ),
           ],
         ),
