778b06c363def5b3f5d47ea59bc069f40b283ff6
Code with Bahri <codewithbahri@gmail.com>
2023-09-20 08:13:21 +0700

    listview.builder

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,10 @@ class ListViewWidget extends StatelessWidget {
     'Living Room',
     'Bed Room',
     'Dining Room',
-    'Kitchen'
+    'Kitchen',
+    'Pool',
+    'Park',
+    'Carport'
   ];
 
   @override
@@ -34,48 +37,35 @@ class ListViewWidget extends StatelessWidget {
       appBar: AppBar(
         title: const Text("ListView"),
       ),
-      body: Container(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 32,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: List.generate(
-                  categories.length,
-                  (index) {
-                    return GestureDetector(
-                      onTap: () {},
-                      child: AnimatedContainer(
-                        duration: const Duration(milliseconds: 150),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.grey,
-                        ),
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 8,
-                          horizontal: 24,
-                        ),
-                        margin: const EdgeInsets.symmetric(horizontal: 2),
-                        child: Text(
-                          categories[index],
-                          textAlign: TextAlign.center,
-                          style: const TextStyle(
-                            fontSize: 12,
-                            color: Colors.black,
-                            fontWeight: FontWeight.w500,
-                            letterSpacing: 1,
-                          ),
-                        ),
-                      ),
-                    );
-                  },
+      body: ListView.builder(
+        itemCount: categories.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {},
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 150),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.grey,
+              ),
+              padding: const EdgeInsets.symmetric(
+                vertical: 8,
+                horizontal: 24,
+              ),
+              margin: const EdgeInsets.symmetric(horizontal: 2, vertical: 2),
+              child: Text(
+                categories[index],
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 12,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w500,
+                  letterSpacing: 1,
                 ),
               ),
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
